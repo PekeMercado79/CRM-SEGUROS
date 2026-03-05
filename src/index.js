23407bc4ed4479f4031b29f2477996ea4ee22f87
PekeMercado79 <jgmercadorodriguez@gmail.com>
2026-03-04 19:41:34 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CRMSeguros from "./crm-seguros-v14";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <CRMSeguros />
+  </React.StrictMode>
+);

20f53b942bad45739265be3d1935d67a7ae7bb79
PekeMercado79 <jgmercadorodriguez@gmail.com>
2026-03-25 06:32:44 -0600

    feat: agregar ruta /admin para panel de administracion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CRMSeguros from "./crm-seguros-v14";
+import AdminPanel from "./AdminPanel";
+
+const esAdmin = window.location.pathname === "/admin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <CRMSeguros />
+    <React.StrictMode>
+{esAdmin ? <AdminPanel /> : <CRMSeguros />}
   </React.StrictMode>
 );
